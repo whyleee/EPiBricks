1e5143eb7ff7199643e5232b1fdfd55a347d875c
Pavel Nezhencev <whyleee@gmail.com>
2013-06-21 01:36:50 +0300

    Link friendly URLs rewriting

---
+++ EPiBricks/Rendering/LinkCollectionFriendlyUrlRewriter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Routing;
+using EPiGlue.Framework;
+using EPiGlue.Helpers;
+using Perks;
+
+namespace EPiBricks.Rendering
+{
+    public class LinkCollectionFriendlyUrlRewriter : IModelPropertyHandler
+    {
+        protected readonly FriendlyUrlRewriter _urlRewriter;
+
+        public LinkCollectionFriendlyUrlRewriter(FriendlyUrlRewriter urlRewriter)
+        {
+            Ensure.ArgumentNotNull(urlRewriter, "urlRewriter");
+
+            _urlRewriter = urlRewriter;
+        }
+
+        public virtual bool CanHandle(ModelPropertyContext context)
+        {
+            return context.Property.PropertyType.Is<IEnumerable<Link>>();
+        }
+
+        public virtual void Process(ModelPropertyContext context)
+        {
+            var links = (IEnumerable<Link>) context.PropertyValue;
+
+            if (links == null)
+            {
+                return;
+            }
+
+            foreach (var link in links)
+            {
+                link.Url = _urlRewriter.GetFriendlyUrl(link.Url, context.ExecutionContext.RequestContext, RouteTable.Routes);
+            }
+        }
+    }
+}
---
+++ EPiBricks/Rendering/LinkFriendlyUrlRewriter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Routing;
+using EPiGlue.Framework;
+using EPiGlue.Helpers;
+using Perks;
+
+namespace EPiBricks.Rendering
+{
+    public class LinkFriendlyUrlRewriter : IModelPropertyHandler
+    {
+        protected readonly FriendlyUrlRewriter _urlRewriter;
+
+        public LinkFriendlyUrlRewriter(FriendlyUrlRewriter urlRewriter)
+        {
+            Ensure.ArgumentNotNull(urlRewriter, "urlRewriter");
+
+            _urlRewriter = urlRewriter;
+        }
+
+        public virtual bool CanHandle(ModelPropertyContext context)
+        {
+            return context.Property.PropertyType.Is<Link>();
+        }
+
+        public virtual void Process(ModelPropertyContext context)
+        {
+            var link = (Link) context.PropertyValue;
+
+            if (link == null)
+            {
+                return;
+            }
+
+            link.Url = _urlRewriter.GetFriendlyUrl(link.Url, context.ExecutionContext.RequestContext, RouteTable.Routes);
+        }
+    }
+}
